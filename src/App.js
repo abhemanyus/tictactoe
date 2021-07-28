08a989bb154f15ad8fb0f1f2df4fa4aec4777329
Aaron Smith <abhemanyus@gmail.com>
2021-07-28 12:15:16 +0530

    add app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {"good": "Fine."};
+  }
+  onClick = (extra) => {
+    console.log(this.state.good);
+    console.log(extra);
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        <button style={{"margin": "1em", 
+                        "padding": "1em", 
+                        "color": "red", 
+                        "backgroundColor": "black", 
+                        "fontSize": "large",
+                        "border": "none"}} 
+                        onClick={this.onClick.bind(this, "hohoho")}>This</button>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
